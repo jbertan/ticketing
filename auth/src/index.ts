0e0b060bf04ae8b22c3ab32b1babd337c6228351
jbertan <bertanakarsu@gmail.com>
2023-01-10 16:21:23 +0300

    Test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  console.log("Starting ...");
+  console.log("Starting .....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT key is not set");
   }

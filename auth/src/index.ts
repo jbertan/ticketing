e255d9cb611d15fb7b7019be7aec82611450fafd
jbertan <bertanakarsu@gmail.com>
2023-01-10 15:52:28 +0300

    Erased s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  console.log("Starting Up ...s");
+  console.log("Starting Up ...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT key is not set");
   }

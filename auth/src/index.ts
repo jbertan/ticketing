39f8e92ae0d4d0c253c46b362e14926c44515465
jbertan <bertanakarsu@gmail.com>
2023-01-10 15:47:40 +0300

    Add s to console.log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  console.log("Starting Up ....");
+  console.log("Starting Up ...s");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT key is not set");
   }
